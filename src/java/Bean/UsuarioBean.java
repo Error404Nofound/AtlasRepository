a1e6038fa83085983ca925b82a626d7ab21a503e
camel <camel@Gary>
2019-06-06 21:21:16 -0500

    Se crearon vistas (Administrar y crear usuario), bean y bussines usuario

---
+++ src/java/Bean/UsuarioBean.java
@@ -0,0 +1,66 @@
+
+package Bean;
+
+
+import Business.UsuarioBusiness;
+import Dao.UsuarioDao;
+import Model.Usuario;
+import java.io.IOException;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author camel
+ */
+@ManagedBean
+@ViewScoped
+
+public class UsuarioBean {
+    
+    List<Usuario> listaUsuarios;
+    private Usuario usuario;
+    
+    public UsuarioBean() {
+        usuario = new Usuario();
+    }
+
+    public List<Usuario> getListaUsuarios() {
+        UsuarioDao usuarioNegocio = new UsuarioBusiness();
+        this.listaUsuarios = usuarioNegocio.listaUsuarios();
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    public void crearUsuario() throws IOException{
+    
+        UsuarioDao usuarioNegocio = new UsuarioBusiness();
+        Usuario usuarioLogeado = (Usuario)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+        
+        Usuario usuarioCreado = new Usuario();
+        usuario.setIdUsuarioModifica(usuarioLogeado.getIdUsuario());
+//        persona.setNombreUsuarioModifica(usuarioLogeado.getPersona().getPrimerNombre());
+        usuarioCreado = usuarioNegocio.crearUsuario(usuario);
+        if(usuarioCreado != null)
+        {
+           FacesContext.getCurrentInstance().getExternalContext().redirect("CrearUsuario.xhtml");
+        }
+        
+    }
+    
+     
+}
--- src/java/Business/UsuarioBusiness.java
+++ src/java/Business/UsuarioBusiness.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Business;
 
 import Dao.UsuarioDao;
 import Model.Usuario;
+import Persistence.NewHibernateUtil;
 import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 /**
  *
@@ -17,7 +16,31 @@ public class UsuarioBusiness  implements UsuarioDao{
 
     @Override
     public Usuario crearUsuario(Usuario usuarioCrear) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                     Session session = null;
+        try
+        {
+            java.sql.Date fechaActual = new java.sql.Date(0, 0, 0);
+             
+            usuarioCrear.setFechaModificacio(fechaActual);
+            usuarioCrear.setFechaContrasena(fechaActual);
+            session = NewHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(usuarioCrear);
+            session.getTransaction().commit();
+        }catch(Exception ex)
+        {
+            System.err.println(ex.getMessage());
+            session.getTransaction().rollback();
+            usuarioCrear = null;
+        }finally
+        {
+            if(session !=null)
+            {
+                session.close();
+            }
+        }
+        
+        return usuarioCrear;
     }
 
     @Override
@@ -27,7 +50,26 @@ public class UsuarioBusiness  implements UsuarioDao{
 
     @Override
     public List<Usuario> listaUsuarios() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Session session = null;
+        
+        List <Usuario> listaUsuario = null;
+        try {
+            session = NewHibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("FROM Usuario");
+            listaUsuario = (List<Usuario>) query.list();
+            
+            
+        }catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }finally
+        {
+        
+            if(session !=null){
+                session.close();
+            }
+        }
+        return listaUsuario;
     }
 
     @Override
